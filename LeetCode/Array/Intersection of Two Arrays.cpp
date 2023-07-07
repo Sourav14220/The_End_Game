49f8a9284c8d42300128126a154a678183659b53
Sourav Halder <66763436+Sourav14220@users.noreply.github.com>
2023-07-07 13:08:14 +0600

    Create Intersection of Two Arrays.cpp

---
+++ LeetCode/Array/Intersection of Two Arrays.cpp
@@ -0,0 +1,17 @@
+//TC : O(N)
+//MC : O(N + M)
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+     unordered_set <int> mst1(nums1.begin( ), nums1.end( )), mst2(nums2.begin( ), nums2.end( ));
+     vector <int> intersect;
+     for(auto it = mst1.begin( ) ; it != mst1.end( ); it++) {
+      int key = *it;
+      if(mst2.find( key ) != mst2.end( )) {
+        intersect.push_back( key );
+        mst2.erase( key );
+      }
+     }
+     return intersect;
+    }
+};
