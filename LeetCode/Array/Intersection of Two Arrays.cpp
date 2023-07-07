9dd34a7d66b615272147f8a72f066c58ca502fff
Sourav Halder <66763436+Sourav14220@users.noreply.github.com>
2023-07-07 13:18:12 +0600

    Update Intersection of Two Arrays.cpp

--- LeetCode/Array/Intersection of Two Arrays.cpp
+++ LeetCode/Array/Intersection of Two Arrays.cpp
@@ -3,15 +3,14 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-     unordered_set <int> mst1(nums1.begin( ), nums1.end( )), mst2(nums2.begin( ), nums2.end( ));
-     vector <int> intersect;
-     for(auto it = mst1.begin( ) ; it != mst1.end( ); it++) {
-      int key = *it;
-      if(mst2.find( key ) != mst2.end( )) {
-        intersect.push_back( key );
-        mst2.erase( key );
-      }
+     vector <int> intersects;
+     unordered_set <int> st(nums1.begin( ), nums1.end( ));
+     for(int i = 0 ; i < nums2.size( ) ; i++) {
+       if(st.find( nums2[i] ) != st.end( )) {
+         intersects.push_back( nums2[i] );
+         st.erase( nums2[i] );
+       }
      }
-     return intersect;
+     return intersects;
     }
 };
